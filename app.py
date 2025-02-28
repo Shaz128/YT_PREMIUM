396e4d642dd6bc9769992fa46784ce06c68601b9
Shaz Mehboob <86738694+Shaz128@users.noreply.github.com>
2025-02-28 14:33:07 +0500

    Update app.py

--- app.py
+++ app.py
@@ -61,6 +61,10 @@ def get_related_videos(video_id, max_results=5):
 
 def download_mp3(youtube_url):
     """Downloads a YouTube video as MP3 using yt-dlp"""
+
+
+ffmpeg_path = ffmpeg.get_ffmpeg_exe()
+
     try:
         ydl_opts = {
             "format":
@@ -69,7 +73,9 @@ def download_mp3(youtube_url):
                 "key": "FFmpegExtractAudio",
                 "preferredcodec": "mp3",
                 "preferredquality": "128"
-            }],  # Faster, better quality
+            }],  
+               ffmpeg_location": ffmpeg_path
+# Faster, better quality
             "noplaylist":
             True,
             "quiet":
